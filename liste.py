85c2c2c4f58210867fafd5e16140d36c62a9031c
Patchouf <112949840+Patchouf@users.noreply.github.com>
2024-01-08 22:52:34 +0100

    Update liste.py

--- liste.py
+++ liste.py
@@ -31,9 +31,6 @@ class ChainedList:
             string = string + str(current_node.next_node.data) + ",\n" 
             current_node = current_node.next_node
         return string
-    
-    # def len_node(self):
-    #     return self.length
         
     def append(self, data):
         self.length += 1
@@ -54,46 +51,6 @@ class ChainedList:
             current_node = current_node.next_node
         return current_node.data
     
-    def create_data(self, data):
-        current_node = self.first_node
-        while current_node is not None:
-            if current_node.data == data:
-                return True
-            current_node = current_node.next_node
-        return False
-    
     def clear_hist(self):
         self.first_node = None
         self.length = 0
-    
-    # def insert_data(self, id, data):
-    #     if id > self.length or id < 0:
-    #         return
-    #     self.length += 1
-    #     if id == 0:
-    #         seconde_node = self.first_node
-    #         self.first_node = NodeList(data, seconde_node)
-    #         return
-    #     avant_node = self.first_node
-    #     current_node = self.first_node.next_node
-    #     while id != 1:
-    #         id -= 1
-    #         avant_node = current_node
-    #         current_node = current_node.next_node
-    #     avant_node.next_node = NodeList(data, current_node)
-        
-    # def remove_data(self, id):
-    #     if id > self.length or id < 0:
-    #         return
-    #     self.length -= 1
-    #     if id == 0:
-    #         self.first_node = self.first_node.next_node
-    #     current_node = self.first_node.next_node
-    #     avant_node = self.first_node
-    #     next_node = self.first_node.next_node
-    #     while id != 0:
-    #         id -= 1
-    #         avant_node = current_node
-    #         current_node = current_node.next_node
-    #         next_node = current_node.next_node
-    #     avant_node.next_node = next_node
